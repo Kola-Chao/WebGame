9bd5a2d169f15862d8016d3b43a6a1e50c462d2b
Kola <kola.tang@backgardon.com>
2023-05-09 16:52:32 +0800

    入口控制逻辑更新

--- app/src/main/java/com/kola/webgame/MainActivity.kt
+++ app/src/main/java/com/kola/webgame/MainActivity.kt
@@ -11,15 +11,20 @@ import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import coil.load
+import com.blankj.utilcode.util.GsonUtils
 import com.google.android.material.tabs.TabLayout.TabGravity
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import com.kola.webgame.bean.IconConfig
 import com.kola.webgame.databinding.ActivityMainBinding
+import com.kola.webgame.utils.KUtils
 import com.kola.webgame.webview.MyWebViewClient
 import com.spin.ok.gp.OkSpin
+import com.spin.ok.gp.OkSpin.initSDK
 import com.spin.ok.gp.utils.Error
 
 
@@ -30,12 +35,13 @@ class MainActivity : AppCompatActivity(), OkSpin.SpinListener {
 
     //通过ViewBind创建View
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-
+    private var mIconConfig: IconConfig? = null
+    private val userId = OkSpin.getUserId()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 //        initFireBase()
-        initSDK()
+        initOkSpinSDK()
         initView()
     }
 
@@ -52,27 +58,28 @@ class MainActivity : AppCompatActivity(), OkSpin.SpinListener {
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     val updated = task.result
-                    Log.d(
-                        TAG,
-                        "Config params updated: $updated config: ${remoteConfig.getString("config")}"
-                    )
+                    refreshConfig(remoteConfig.getString("config"))
                 } else {
-                    Toast.makeText(
-                        this, "Fetch failed",
-                        Toast.LENGTH_SHORT
-                    ).show()
                 }
                 //                displayWelcomeMessage()
             }
         remoteConfig.setConfigSettingsAsync(configSettings)
+        refreshConfig(remoteConfig.getString("config"))
     }
 
-    private fun initSDK() {
+    private fun refreshConfig(config: String) {
+        mIconConfig = GsonUtils.fromJson(config, IconConfig::class.java)
+        mIconConfig?.url?.let {
+            mIconConfig?.url = KUtils.getInstance().replaceUrl(this, it, userId).toString()
+        }
+    }
+
+    private fun initOkSpinSDK() {
         OkSpin.setListener(this)
         if (!OkSpin.isInit()) {
             OkSpin.initSDK(OkSpin_Key)
             OkSpin.loadIcon(OkSpin_Placement)
-            OkSpin.setUserId(OkSpin.getUserId())
+            OkSpin.setUserId(userId)
         }
     }
 
@@ -85,8 +92,8 @@ class MainActivity : AppCompatActivity(), OkSpin.SpinListener {
 //        MobileAds.registerWebView(binding.web);
         binding.web.loadUrl("https://cashbird.minigame.vip/game/pop-stone3/play?from=home")
         binding.icon.setOnClickListener {
-            if (OkSpin.isGSpaceReady(OkSpin_Placement)) {
-                OkSpin.openGSpace(OkSpin_Placement)
+            if (OkSpin.isInteractiveReady(OkSpin_Placement)) {
+                OkSpin.openInteractive(OkSpin_Placement)
             }
         }
     }
@@ -103,11 +110,20 @@ class MainActivity : AppCompatActivity(), OkSpin.SpinListener {
             ViewGroup.LayoutParams.MATCH_PARENT
         )
         binding.icon.addView(iconView, layoutParams)
+        showGSpaceIcon()
+    }
+
+    private fun showGSpaceIcon() {
+        binding.icon.visibility = View.VISIBLE
     }
 
     override fun onInitSuccess() {
         // 初始化成功
         Log.w(TAG, "onInitSuccess: ")
+        //使用Coil加载图片到binding.ivIcon上
+        binding.ivIcon.load(mIconConfig?.url)
+        binding.ivIcon.visibility = View.VISIBLE
+        showGSpaceIcon()
     }
 
     override fun onInitFailed(error: Error?) {
@@ -123,6 +139,7 @@ class MainActivity : AppCompatActivity(), OkSpin.SpinListener {
 
     override fun onIconLoadFailed(placement: String?, error: Error?) {
         // Placement 加载失败
+
         Log.w(TAG, "onIconLoadFailed: $placement error: $error")
     }
 
