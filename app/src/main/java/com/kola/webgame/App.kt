1b797f2229c33f2eb62c5eefeb134782ce0abe99
Kola <kola.tang@backgardon.com>
2023-06-05 01:46:04 +0800

    feat: 添加了全局的异常捕获

--- app/src/main/java/com/kola/webgame/App.kt
+++ app/src/main/java/com/kola/webgame/App.kt
@@ -3,7 +3,9 @@ package com.kola.webgame
 import android.app.Application
 import coil.ImageLoader
 import coil.ImageLoaderFactory
+import com.blankj.utilcode.util.AppUtils
 import com.blankj.utilcode.util.Utils
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.kola.webgame.utils.KUtils
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
@@ -21,13 +23,13 @@ class App : Application(), ImageLoaderFactory {
         com.ks.vny.lqh.e.a(this)
         KUtils.getInstance().getDeviceGaid(this)
         //如果发生了全局未捕获的异常，可以在这里捕获
-//        Thread.setDefaultUncaughtExceptionHandler { t, e ->
-//            e.printStackTrace()
-//            //上报Firebase异常
-//            com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance().recordException(e)
-//            //重启app
-//            AppUtils.relaunchApp()
-//        }
+        Thread.setDefaultUncaughtExceptionHandler { t, e ->
+            e.printStackTrace()
+            //上报Firebase异常
+            FirebaseCrashlytics.getInstance().recordException(e)
+            //重启app
+            AppUtils.relaunchApp()
+        }
     }
 
     override fun newImageLoader(): ImageLoader = imageLoader.get()
